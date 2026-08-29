dff80106d0f3a15c6829dd8c5dde7775eb34d3ca
Sapalskyi Oleksandr <o.sapalskyi@andersenlab.com>
2026-08-29 13:41:31 +0300

    base app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>React + TypeScript + Vite</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to get started.
+      </p>
+    </main>
+  )
+}
+
+export default App
